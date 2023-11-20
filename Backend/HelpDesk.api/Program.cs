da9165dd90022ad8ea86b9ac988fc3bfbb212d1f
Jeff Gonzalez <Jeff@hypertheory.com>
2023-11-20 10:17:26 -0500

    Add OIDC Configuration

--- Backend/HelpDesk.api/Program.cs
+++ Backend/HelpDesk.api/Program.cs
@@ -4,9 +4,36 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 });
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddBff(options =>
+{
+    options.ManagementBasePath = "/api";
+});
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "cookie";
+    options.DefaultChallengeScheme = "oidc";
+    options.DefaultSignOutScheme = "oidc";
+}).AddCookie("cookie", options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromHours(8);
+    options.SlidingExpiration = false;
+    options.Cookie.Name = "__Host-issue-tracker";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+}).AddOpenIdConnect("oidc", options =>
+{
+
+});
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseBff();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapBffManagementEndpoints();
+app.UseAuthorization();
 
 if(app.Environment.IsDevelopment())
 {
