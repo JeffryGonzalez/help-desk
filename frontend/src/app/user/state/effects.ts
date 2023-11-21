0532da2c80a24ac4ec33158e1a8c4042f89c10af
Jeff Gonzalez <jeff@hypertheory.com>
2023-11-20 20:36:38 -0500

    about to do the effect

--- frontend/src/app/user/state/effects.ts
+++ frontend/src/app/user/state/effects.ts
@@ -5,9 +5,9 @@ import { Store } from '@ngrx/store';
 
 import { map, switchMap, tap } from 'rxjs';
 import { UserState } from '.';
-import { UserDocuments } from './actions';
 import { authFeature, getApiUrl } from '../../auth/state';
 import { AuthEvents } from '../../auth/state/actions';
+import { UserContactCommands, UserDocuments } from './actions';
 
 @Injectable()
 export class UserEffects {
@@ -27,9 +27,32 @@ export class UserEffects {
       ),
     { dispatch: true }
   );
+
+  updateUserData$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(UserContactCommands.updateItem),
+        concatLatestFrom(() => this.store.select(authFeature.selectStreamId)),
+        map(([action, streamId]) => ({ ...action, streamId })),
+        tap((a) => {
+          const path = this.camelToKebab(a.payload.operation);
+          const url = this.url + `users/${a.streamId}/${path}`;
+          const body = { value: a.payload.value[a.payload.operation] };
+          console.log(url, body);
+        })
+      ),
+    {
+      dispatch: false,
+    }
+  );
   constructor(
     private readonly actions$: Actions,
     private readonly http: HttpClient,
     private readonly store: Store
   ) {}
+
+  camelToKebab(path: string) {
+    // convert path to kebab-case
+    return path.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+  }
 }
