8b62e498d33be640e3d5b62662da5d649f68afae
Jeff Gonzalez <Jeff@hypertheory.com>
2023-12-05 14:12:40 -0500

    more issues stuff

--- src/Frontend/src/app/features/techs/techs.component.ts
+++ src/Frontend/src/app/features/techs/techs.component.ts
@@ -5,41 +5,44 @@ import { Signal } from '@ngrx/signals/src/deep-signal';
 import { ProfileService } from '../user/features/profile/services';
 
 @Component({
-    selector: 'app-techs',
-    standalone: true,
-    template: `
-   @if(unassigned().isLoading) {
+  selector: 'app-techs',
+  standalone: true,
+  template: `
+    @if(unassigned().isLoading) {
     <div class="loading loading-spinner loading-lg"></div>
-   } @else {
-    @for(i of unassigned().data; track i.id) {
-      <div class="pb-4">
-        <p>{{ i.description }}</p>
-        <p>{{ i.created | date:'fullDate' }}</p>
-        @if(i.customerInfo === undefined) {
-        <button (click)="get(i.customerId)" class="btn btn-primary">Get Customer Info</button>
-        } @else {
-        <div>
-          <p>{{ i.customerInfo.firstName }} {{ i.customerInfo.lastName }}</p>
-          <p>{{ i.customerInfo.emailAddress }}</p>
-          <p>{{ i.customerInfo.phoneNumber }}</p>
-        </div>
-        }
-        
+    } @else { @for(i of unassigned().data; track i.id) {
 
+    <details class="collapse bg-base-200 mb-4">
+      <summary
+        class="collapse-title text-xl font-medium"
+        (click)="get(i.customerId)"
+      >
+        {{ i.description }}
+        <span class="text-sm">{{ i.created | date : 'fullDate' }}</span>
+        <div>
+          <button class="btn btn-accent btn-sm">Take This Issue</button>
+        </div>
+      </summary>
+      <div class="collapse-content">
+        @if(i.customerInfo) {
+        <p>{{ i.customerInfo.firstName }} {{ i.customerInfo.lastName }}</p>
+        <p>{{ i.customerInfo.emailAddress }}</p>
+        <p>{{ i.customerInfo.phoneNumber }}</p>
+        }
       </div>
-    }  
-   }
-  
+    </details>
+
+    } }
   `,
-    styles: ``,
-    imports: [JsonPipe, DatePipe]
+  styles: ``,
+  imports: [JsonPipe, DatePipe],
 })
 export class TechsComponent {
   private readonly service = inject(UnassignedIncidentsService);
   unassigned = this.service.get().result;
-  private getCustomerInfo = this.service.getContactForIncident()
+  private getCustomerInfo = this.service.getContactForIncident();
 
   get(userId: string) {
-    this.getCustomerInfo.mutate({ userId })
+    this.getCustomerInfo.mutate({ userId });
   }
 }
